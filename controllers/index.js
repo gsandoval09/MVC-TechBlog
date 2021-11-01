5a0e38f35d3e845af6e547e7f5a23d25091b1c05
George Sandoval <gssandoval09@gmail.com>
2021-11-01 00:40:11 -0500

    made homeRoutes folder and added code

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 
-router.use('users', userRoutes);
+router.use('/users', userRoutes);
+// router.use('/homeRoutes', homeRoutes);
 
 module.exports = router;
